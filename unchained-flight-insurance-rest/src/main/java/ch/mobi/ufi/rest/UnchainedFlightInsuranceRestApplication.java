1fc2f8aa531e8d274a3ae913328019e2b4ffa380
von Dach Olivier (extern) <olivier.vondach@mobi.ch>
2018-05-24 18:34:56 +0200

    Initial commit

---
+++ unchained-flight-insurance-rest/src/main/java/ch/mobi/ufi/rest/UnchainedFlightInsuranceRestApplication.java
@@ -0,0 +1,12 @@
+package ch.mobi.ufi.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnchainedFlightInsuranceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnchainedFlightInsuranceRestApplication.class, args);
+	}
+}
---
+++ unchained-flight-insurance-web/src/main/java/ch/mobi/ufi/web/UnchainedFlightInsuranceWebApplication.java
@@ -0,0 +1,12 @@
+package ch.mobi.ufi.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnchainedFlightInsuranceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnchainedFlightInsuranceWebApplication.class, args);
+    }
+}
