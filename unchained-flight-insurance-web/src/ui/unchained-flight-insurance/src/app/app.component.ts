96c42ea97fdabe1d135a33885d9668ca012367ea
Damiot Didier (U113203) <didier.damiot@mobi.ch>
2018-05-28 13:48:06 +0200

    Didier's first commit.

--- unchained-flight-insurance-web/src/ui/unchained-flight-insurance/src/app/app.component.ts
+++ unchained-flight-insurance-web/src/ui/unchained-flight-insurance/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'the Unchained Flight Insurance App!';
 }
