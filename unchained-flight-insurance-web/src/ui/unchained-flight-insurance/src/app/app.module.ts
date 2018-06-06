b398c054e6f62ba767c6b00d88bf0e1d879a9b81
Damiot Didier (U113203) <didier.damiot@mobi.ch>
2018-06-06 12:00:51 +0200

    GUI 1 implementation in progress.

--- unchained-flight-insurance-web/src/ui/unchained-flight-insurance/src/app/app.module.ts
+++ unchained-flight-insurance-web/src/ui/unchained-flight-insurance/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SubscriptionComponent } from './component/subscription/subscription.component';
+import { BillingComponent } from './component/billing/billing.component';
+import {ButtonModule} from 'primeng/button';
+import {CalendarModule, DropdownModule} from 'primeng/primeng';
+import {ListboxModule} from 'primeng/listbox';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscriptionComponent,
+    BillingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    DropdownModule,
+    CalendarModule,
+    ListboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
