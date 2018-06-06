308514c17c504d681eb0f2b163bf3687c5ef4741
vondacho <olivier.vondach@obya.ch>
2018-06-06 14:16:23 +0200

    Refactorin continued, SRP principle

--- unchained-flight-insurance-web/src/ui/unchained-flight-insurance/src/app/app.component.ts
+++ unchained-flight-insurance-web/src/ui/unchained-flight-insurance/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ContractService} from "./service/contract.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the Unchained Flight Insurance App';
+
+  constructor(private contractService: ContractService) {
+  }
 }
