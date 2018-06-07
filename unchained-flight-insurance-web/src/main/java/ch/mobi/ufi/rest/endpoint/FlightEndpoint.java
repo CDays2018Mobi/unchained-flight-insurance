9e7cfd7cfccde1740a271707bda50799de4ea95c
Julien Kronegg <julien@kronegg.ch>
2018-06-07 09:49:44 +0200

    mock /flights/insurable service

--- unchained-flight-insurance-web/src/main/java/ch/mobi/ufi/rest/endpoint/FlightEndpoint.java
+++ unchained-flight-insurance-web/src/main/java/ch/mobi/ufi/rest/endpoint/FlightEndpoint.java
@@ -1,17 +1,28 @@
 package ch.mobi.ufi.rest.endpoint;
 
-import ch.mobi.ufi.domain.flight.entity.Flight;
-import ch.mobi.ufi.domain.flight.repository.FlightCache;
-import ch.mobi.ufi.domain.flight.service.FlightService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import static org.springframework.http.ResponseEntity.ok;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.http.ResponseEntity.ok;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.mobi.ufi.domain.flight.entity.Airline;
+import ch.mobi.ufi.domain.flight.entity.Flight;
+import ch.mobi.ufi.domain.flight.entity.InsurableFlight;
+import ch.mobi.ufi.domain.flight.repository.FlightCache;
+import ch.mobi.ufi.domain.flight.service.FlightService;
+import ch.mobi.ufi.domain.risk.predictor.RiskCoverage;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @CrossOrigin
@@ -37,5 +48,26 @@ public class FlightEndpoint {
         return ok("refresh done: found " + flights.size() + " flights");
     }
 
+    /**
+     * Returns the list of flights that can be insured.
+     * @return
+     */
+    @GetMapping("/insurable")
+    public ResponseEntity<List<InsurableFlight>> getInsurableFlights() {
+    	List<InsurableFlight> mockedFlights = new ArrayList<>();
+    	RiskCoverage rc = RiskCoverage.builder().build();
+    	mockedFlights.add(InsurableFlight.builder()
+    			.flight(Flight.builder()
+    					.airline(Airline.builder().companyName("EASYJET").build())
+    					.flightNumber("EA1234")
+    					.expectedArrivalDate(LocalDateTime.now())
+    					.build())
+    			.delayProbability(0.2)
+    			.riskCoverages(Arrays.asList(rc, rc, rc))
+    			.build());
+//    	lightCache.getFlights(flight -> flight.getExpectedArrivalDate()
+//                .isAfter(LocalDate.now().minusDays(1).atStartOfDay()))
+        return ok(mockedFlights);
+    }
 }
 
