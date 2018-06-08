fb07cf141a1dd5be4279705acb4df34a5b9f9249
vondacho <olivier.vondach@obya.ch>
2018-06-08 12:43:26 +0200

    Didier : payment works !!!!

--- unchained-flight-insurance-web/src/main/java/ch/mobi/ufi/rest/endpoint/DataTransEndpoint.java
+++ unchained-flight-insurance-web/src/main/java/ch/mobi/ufi/rest/endpoint/DataTransEndpoint.java
@@ -1,9 +1,16 @@
 package ch.mobi.ufi.rest.endpoint;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
+import ch.mobi.ufi.domain.contract.service.ContractService;
+import ch.mobi.ufi.domain.flight.vo.FlightIdentifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +19,50 @@ import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/billing")
 public class DataTransEndpoint {
 
+    @Autowired
+    private ContractService contractService;
+
     @PostMapping("/cancelled")
     public ModelAndView cancelled(HttpServletRequest request) {
         return new ModelAndView("redirect:/billing/cancelled?" + request.getQueryString(), Collections.emptyMap());
     }
 
-    @PostMapping("/payed")
+    @PostMapping("/succeeded")
     public ModelAndView payed(HttpServletRequest request) {
-        return new ModelAndView("redirect:/billing/succeeded?" + request.getQueryString(), Collections.emptyMap());
+
+        String queryString = request.getQueryString();
+        String[] fields = queryString.split("&");
+        String[] kv;
+
+        HashMap<String, String> things = new HashMap<String, String>();
+
+
+        for (int i = 0; i < fields.length; ++i) {
+            kv = fields[i].split("=");
+            if (2 == kv.length) {
+                things.put(kv[0], kv[1]);
+            }
+        }
+
+        final String flightId = things.get("flightId");
+        final String arrivalDate = things.get("expectedArrivalDate");
+
+        contractService.createContract(
+                FlightIdentifier.builder()
+                        .flightNumber(flightId)
+                        .flightArrivalDate(LocalDate.parse(arrivalDate, DateTimeFormatter.ISO_DATE))
+                        .build(),
+                Duration.ofMinutes(10));
+
+        return new ModelAndView("redirect:/billing/succeeded?" + queryString, Collections.emptyMap());
     }
 }
 
